813a7f50228f4c340fad455ddb7140803e6f6b6d
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-29 23:06:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,13 +4,13 @@ require 'directors_database'
 def gross_for_director(director_data)
   movie_index = 0 
   director_index = 0 
-  
   while movie_index < directors_database[director_index].length do
-    director_total += directors_database[director_index][:movies][movie_index][:worldwide_gross]
+    director_total = directors_database[director_index][:movies][movie_index][:worldwide_gross]
     movie_index += 1 
   end
   director_index += 1 
 end 
+
 
 def directors_totals(nds)
   name_index = 0
