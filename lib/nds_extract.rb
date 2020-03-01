89ae5958bb1f790184055b1f6e5dfe9478aa180e
Gabrielle Noel <gabby@gabriellealexa.com>
2020-03-01 00:25:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ def directors_totals(nds)
   
   while director_index < nds.size do
     director = nds[director_index]
-    result[director[:name]] += gross_for_director(director)
+    result[director[:name]] = gross_for_director(director)
     director_index += 1
     end 
   result 
